9b78f255be6c2066665095f6d16f2d4d292d015b
Yuanfang <yuan-fang1@hotmail.com>
2022-07-14 04:38:45 +0000

    vm

--- virtual-machine.tf
+++ virtual-machine.tf
@@ -10,7 +10,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = tls_private_key.ssh-key.publc_key_openssh
+    public_key = tls_private_key.ssh-key.public_key_openssh
   }
 
   os_disk {
